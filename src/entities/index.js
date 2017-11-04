842ea2861c9e416c1b1beaa8fb2329780c92aa36
Roman Pushkov <pushkovroman@me.com>
2017-11-04 07:04:02 +0300

    testing issue

--- src/entities/index.js
+++ src/entities/index.js
@@ -13,6 +13,9 @@ export default () => {
     process.env[config.use_env_variable] :
     config.database, config.username, config.password, config);
 
+  console.log(config.use_env_variable ? config.use_env_variable : '?');
+  console.log(sequelize);
+
   fs
     .readdirSync(__dirname)
     .filter(file => ((file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')))
