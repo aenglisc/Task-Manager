70f2893bb886a7f74532e9cb7d7f65ac640dd39f
Roman Pushkov <pushkovroman@me.com>
2017-11-04 04:47:32 +0300

    routing index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,11 @@
 import home from './home';
+import users from './users';
+import sessions from './sessions';
 
 const controllers = [
   home,
+  users,
+  sessions,
 ];
 
-export default (router, container) => controllers.forEach(item => item(router, container));
+export default (router, container) => controllers.forEach(f => f(router, container));
