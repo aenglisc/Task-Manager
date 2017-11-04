48072f4b6246e468efe855f241dc8329f139a2a3
Roman Pushkov <pushkovroman@me.com>
2017-11-04 04:43:49 +0300

    home route

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,6 +1,10 @@
-export default (router) => {
-  router.get('root', '/', (ctx) => {
-    // ctx.body = 'Hello World';
-    ctx.render('home/index');
+import log from '../lib/logger';
+
+export default (router, { User }) => {
+  router.get('home', '/', async (ctx) => {
+    log('Rendering the home page');
+    log(ctx.state.id);
+    const user = await User.findById(ctx.state.id);
+    ctx.render('home/index', { user });
   });
 };
