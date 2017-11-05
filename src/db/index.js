bb79aea196e7390d406074baaf4b57d56c56739f
Roman Pushkov <pushkovroman@me.com>
2017-11-05 18:47:28 +0300

    db fix

--- src/db/index.js
+++ src/db/index.js
@@ -7,4 +7,6 @@ dotenv.config();
 const env = process.env.NODE_ENV;
 const config = configfile[env];
 
-export default new Sequelize(config);
+export default new Sequelize(config.use_env_variable ?
+  process.env[config.use_env_variable] :
+  config);
