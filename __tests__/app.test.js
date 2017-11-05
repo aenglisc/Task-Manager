367f955f65fd4e20fd799484d4c14889f98bca7a
Roman Pushkov <pushkovroman@me.com>
2017-11-05 18:21:59 +0300

    added user session tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,33 +1,85 @@
+/* eslint object-curly-newline: ["error", { "minProperties": 5 }] */
+
 import request from 'supertest';
 import matchers from 'jest-supertest-matchers';
+import faker from 'faker';
 
 import app from '../src';
+import init from '../src/init';
 
-describe('requests', () => {
-  let server;
+const createUser = id => ({
+  id,
+  email: faker.internet.email(),
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  password: faker.internet.password(),
+});
 
-  beforeAll(() => {
+describe('Basic', () => {
+  const server = app().listen();
+
+  beforeAll(async () => {
     jasmine.addMatchers(matchers);
   });
 
-  beforeEach(() => {
-    server = app().listen();
+  afterAll((done) => {
+    server.close();
+    done();
   });
 
-  it('GET 200', async () => {
-    const res = await request.agent(server)
-      .get('/');
+  it('Homepage', async () => {
+    const res = await request.agent(server).get('/');
     expect(res).toHaveHTTPStatus(200);
   });
 
-  it('GET 404', async () => {
-    const res = await request.agent(server)
-      .get('/wrong-path');
+  it('404', async () => {
+    const res = await request.agent(server).get('/void');
     expect(res).toHaveHTTPStatus(404);
   });
+});
 
-  afterEach((done) => {
+describe('User session', () => {
+  const { email, firstName, lastName, password } = createUser(1);
+  const server = app().listen();
+
+  beforeAll(async () => {
+    jasmine.addMatchers(matchers);
+    await init();
+  });
+
+  afterAll((done) => {
     server.close();
     done();
+  });
+
+  it('Sign up', async () => {
+    await request
+      .agent(server)
+      .post('/users')
+      .type('form')
+      .send({ form: { email, firstName, lastName, password } })
+      .expect(302);
+  });
+
+  it('Sign in', async () => {
+    await request
+      .agent(server)
+      .post('/sessions')
+      .type('form')
+      .send({ form: { email, password } })
+      .expect(302);
+  });
+
+  it('Sign out', async () => {
+    await request
+      .agent(server)
+      .post('/sessions')
+      .type('form')
+      .send({ form: { email, password } })
+      .expect(302);
+    await request
+      .agent(server)
+      .delete('/sessions')
+      .expect(302);
   });
 });
